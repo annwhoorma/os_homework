1357a644e47313edd965b0488299ff9d452f562b
annwhoorma <anyaboronina59@gmail.com>
2019-09-12 20:03:01 +0300

    commit

--- week04/exercise1/ex1.sh
+++ week04/exercise1/ex1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-for i in 1 .. 10
+[ -e ex1_output.txt ] && unlink ex1_output.txt
+for i in 1 2 3 4 5 6 7 8 9 10
 do
-	gcc ex1.c -o ex1 && ./ex1 >> ex1_output.txt
+	gcc ex1.c -o ex1
+	./ex1 >> ex1_output.txt
 done
